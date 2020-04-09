4124b3ec74521e12e619ed96875446b3858fbd34
Godfrey Lebo <emorylebo@gmail.com>
2020-04-09 19:26:25 +0100

    Initial commit

---
+++ src/com/chess/engine/classic/pieces/Piece.java
@@ -0,0 +1,235 @@
+package com.chess.engine.classic.pieces;
+
+import com.chess.engine.classic.Alliance;
+import com.chess.engine.classic.board.Board;
+import com.chess.engine.classic.board.Move;
+
+import java.util.Collection;
+
+public abstract class Piece {
+
+    final PieceType pieceType;
+    final Alliance pieceAlliance;
+    final int piecePosition;
+    private final boolean isFirstMove;
+    private final int cachedHashCode;
+
+    Piece(final PieceType type,
+          final Alliance alliance,
+          final int piecePosition,
+          final boolean isFirstMove) {
+        this.pieceType = type;
+        this.piecePosition = piecePosition;
+        this.pieceAlliance = alliance;
+        this.isFirstMove = isFirstMove;
+        this.cachedHashCode = computeHashCode();
+    }
+
+    public PieceType getPieceType() {
+        return this.pieceType;
+    }
+
+    public Alliance getPieceAllegiance() {
+        return this.pieceAlliance;
+    }
+
+    public int getPiecePosition() {
+        return this.piecePosition;
+    }
+
+    public boolean isFirstMove() {
+        return this.isFirstMove;
+    }
+
+    public int getPieceValue() {
+        return this.pieceType.getPieceValue();
+    }
+
+    public abstract int locationBonus();
+
+    public abstract Piece movePiece(Move move);
+
+    public abstract Collection<Move> calculateLegalMoves(final Board board);
+
+
+    @Override
+    public boolean equals(final Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof Piece)) {
+            return false;
+        }
+        final Piece otherPiece = (Piece) other;
+        return this.piecePosition == otherPiece.piecePosition && this.pieceType == otherPiece.pieceType &&
+               this.pieceAlliance == otherPiece.pieceAlliance && this.isFirstMove == otherPiece.isFirstMove;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.cachedHashCode;
+    }
+
+    private int computeHashCode() {
+        int result = this.pieceType.hashCode();
+        result = 31 * result + this.pieceAlliance.hashCode();
+        result = 31 * result + this.piecePosition;
+        result = 31 * result + (this.isFirstMove ? 1 : 0);
+        return result;
+    }
+
+    public enum PieceType {
+
+        PAWN(100, "P") {
+            @Override
+            public boolean isPawn() {
+                return true;
+            }
+
+            @Override
+            public boolean isBishop() {
+                return false;
+            }
+
+            @Override
+            public boolean isRook() {
+                return false;
+            }
+
+            @Override
+            public boolean isKing() {
+                return false;
+            }
+        },
+        KNIGHT(320, "N") {
+            @Override
+            public boolean isPawn() {
+                return false;
+            }
+
+            @Override
+            public boolean isBishop() {
+                return false;
+            }
+
+            @Override
+            public boolean isRook() {
+                return false;
+            }
+
+            @Override
+            public boolean isKing() {
+                return false;
+            }
+        },
+        BISHOP(350, "B") {
+            @Override
+            public boolean isPawn() {
+                return false;
+            }
+
+            @Override
+            public boolean isBishop() {
+                return true;
+            }
+
+            @Override
+            public boolean isRook() {
+                return false;
+            }
+
+            @Override
+            public boolean isKing() {
+                return false;
+            }
+        },
+        ROOK(500, "R") {
+            @Override
+            public boolean isPawn() {
+                return false;
+            }
+
+            @Override
+            public boolean isBishop() {
+                return false;
+            }
+
+            @Override
+            public boolean isRook() {
+                return true;
+            }
+
+            @Override
+            public boolean isKing() {
+                return false;
+            }
+        },
+        QUEEN(900, "Q") {
+            @Override
+            public boolean isPawn() {
+                return false;
+            }
+
+            @Override
+            public boolean isBishop() {
+                return false;
+            }
+
+            @Override
+            public boolean isRook() {
+                return false;
+            }
+
+            @Override
+            public boolean isKing() {
+                return false;
+            }
+        },
+        KING(20000, "K") {
+            @Override
+            public boolean isPawn() {
+                return false;
+            }
+
+            @Override
+            public boolean isBishop() {
+                return false;
+            }
+
+            @Override
+            public boolean isRook() {
+                return false;
+            }
+
+            @Override
+            public boolean isKing() {
+                return true;
+            }
+        };
+
+        private final int value;
+        private final String pieceName;
+
+        public int getPieceValue() {
+            return this.value;
+        }
+
+        @Override
+        public String toString() {
+            return this.pieceName;
+        }
+
+        PieceType(final int val,
+                  final String pieceName) {
+            this.value = val;
+            this.pieceName = pieceName;
+        }
+
+        public abstract boolean isPawn();
+        public abstract boolean isBishop();
+        public abstract boolean isRook();
+        public abstract boolean isKing();
+
+    }
+
+}
---
+++ src/com/chess/engine/classic/player/ai/StandardBoardEvaluator.java
@@ -0,0 +1,109 @@
+package com.chess.engine.classic.player.ai;
+
+import com.chess.engine.classic.board.Board;
+import com.chess.engine.classic.board.Move;
+import com.chess.engine.classic.pieces.Piece;
+import com.chess.engine.classic.player.Player;
+import com.chess.engine.classic.player.ai.KingSafetyAnalyzer.KingDistance;
+import com.google.common.annotations.VisibleForTesting;
+
+public final class StandardBoardEvaluator
+        implements BoardEvaluator {
+
+    private final static int CHECK_MATE_BONUS = 100000;
+    private final static int CHECK_BONUS = 20;
+    private final static int CASTLE_BONUS = 40;
+    private final static int MOBILITY_MULTIPLIER = 2;
+    private final static int ATTACK_MULTIPLIER = 2;
+    private final static int TWO_BISHOPS_BONUS = 50;
+    private static final StandardBoardEvaluator INSTANCE = new StandardBoardEvaluator();
+
+    private StandardBoardEvaluator() {
+    }
+
+    public static StandardBoardEvaluator get() {
+        return INSTANCE;
+    }
+
+    @Override
+    public int evaluate(final Board board,
+                        final int depth) {
+        return score(board.whitePlayer(), depth) - score(board.blackPlayer(), depth);
+    }
+
+    @VisibleForTesting
+    private static int score(final Player player,
+                             final int depth) {
+        return mobility(player) +
+               kingThreats(player, depth) +
+               attacks(player) +
+               castle(player) +
+               pieceEvaluations(player) +
+               pawnStructure(player);
+    }
+
+    private static int attacks(final Player player) {
+        int attackScore = 0;
+        for(final Move move : player.getLegalMoves()) {
+            if(move.isAttack()) {
+                final Piece movedPiece = move.getMovedPiece();
+                final Piece attackedPiece = move.getAttackedPiece();
+                if(movedPiece.getPieceValue() <= attackedPiece.getPieceValue()) {
+                    attackScore++;
+                }
+            }
+        }
+        return attackScore * ATTACK_MULTIPLIER;
+    }
+
+    private static int pieceEvaluations(final Player player) {
+        int pieceValuationScore = 0;
+        int numBishops = 0;
+        for (final Piece piece : player.getActivePieces()) {
+            pieceValuationScore += piece.getPieceValue() + piece.locationBonus();
+            if(piece.getPieceType().isBishop()) {
+                numBishops++;
+            }
+        }
+        return pieceValuationScore + (numBishops == 2 ? TWO_BISHOPS_BONUS : 0);
+    }
+
+    private static int mobility(final Player player) {
+        return MOBILITY_MULTIPLIER * mobilityRatio(player);
+    }
+
+    private static int mobilityRatio(final Player player) {
+        return (int)((player.getLegalMoves().size() * 100.0f) / player.getOpponent().getLegalMoves().size());
+    }
+
+    private static int kingThreats(final Player player,
+                                   final int depth) {
+        return player.getOpponent().isInCheckMate() ? CHECK_MATE_BONUS  * depthBonus(depth) : check(player);
+    }
+
+    private static int check(final Player player) {
+        return player.getOpponent().isInCheck() ? CHECK_BONUS : 0;
+    }
+
+    private static int depthBonus(final int depth) {
+        return depth == 0 ? 1 : 100 * depth;
+    }
+
+    private static int castle(final Player player) {
+        return player.isCastled() ? CASTLE_BONUS : 0;
+    }
+
+    private static int pawnStructure(final Player player) {
+        return PawnStructureAnalyzer.get().pawnStructureScore(player);
+    }
+
+    private static int kingSafety(final Player player) {
+        final KingDistance kingDistance = KingSafetyAnalyzer.get().calculateKingTropism(player);
+        return ((kingDistance.getEnemyPiece().getPieceValue() / 100) * kingDistance.getDistance());
+    }
+
+    private static int rookStructure(final Board board, final Player player) {
+        return RookStructureAnalyzer.get().rookStructureScore(board, player);
+    }
+
+}
